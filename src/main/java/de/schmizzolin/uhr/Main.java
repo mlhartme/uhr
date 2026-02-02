65cf4f02432080764b30576bae3c3fd51baf1eb7
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-02 20:01:59 +0100

    feat: runs via maven

--- src/main/java/de/schmizzolin/uhr/Main.java
+++ src/main/java/de/schmizzolin/uhr/Main.java
@@ -1,10 +1,21 @@
 package de.schmizzolin.uhr;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Label helloLabel = new Label("Hello World");
+        Scene scene = new Scene(helloLabel, 300, 200);
+        primaryStage.setTitle("JavaFX Hello World");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    private Main() {
+    public static void main(String[] args) {
+        launch(args);
     }
 }
