de2cc19ffe948a60058bf2fd8bb495fce38be8e3
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-02 21:53:12 +0100

    real time

--- src/main/java/de/schmizzolin/uhr/Main.java
+++ src/main/java/de/schmizzolin/uhr/Main.java
@@ -4,11 +4,16 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.time.LocalTime;
+import java.util.Locale;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        LocalTime time = LocalTime.now();
         Font font = Font.create();
-        Scene scene = new Scene(font.render("17:23"), 300, 200);
+        String str = String.format(Locale.GERMAN, "%02d:%02d", time.getHour(), time.getMinute());
+        Scene scene = new Scene(font.render(str), 300, 200);
         primaryStage.setTitle("Uhr");
         primaryStage.setScene(scene);
         primaryStage.show();
