50893a70b1713506895e761cc7874f00568983b5
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-02 19:44:49 +0100

    initial release

---
+++ src/main/java/de/schmizzolin/uhr/Main.java
@@ -0,0 +1,10 @@
+package de.mlhartung.uhr;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    private Nain() {
+    }
+}
