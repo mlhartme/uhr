b0608c1b9f23f91621c6da40c57f45394588b65b
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-02 19:48:49 +0100

    hello works

--- src/main/java/de/schmizzolin/uhr/Main.java
+++ src/main/java/de/schmizzolin/uhr/Main.java
@@ -1,10 +1,10 @@
-package de.mlhartung.uhr;
+package de.schmizzolin.uhr;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 
-    private Nain() {
+    private Main() {
     }
 }
