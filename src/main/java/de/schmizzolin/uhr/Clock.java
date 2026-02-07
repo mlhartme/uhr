87033a184422f8f46a54045ced85e13661387cbe
Michael Hartmeier <mlhartme.users.noreply@github.com>
2026-02-07 10:28:24 +0100

    clock pane

---
+++ src/main/java/de/schmizzolin/uhr/Clock.java
@@ -0,0 +1,48 @@
+package de.schmizzolin.uhr;
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+import java.time.LocalTime;
+import java.util.Locale;
+
+// Beispiel für eine eigene Pane mit automatischer Aktualisierung
+public class Clock extends StackPane {
+    private final Font font;
+
+    public Clock() {
+        this.font = Font.create(Color.WHITE);
+        setAlignment(Pos.CENTER);
+        setStyle("-fx-background-color: black;");
+        setBorder(new Border(new BorderStroke(
+                Color.BLACK,
+                BorderStrokeStyle.SOLID,
+                null,
+                new BorderWidths(10)
+        )));
+        refresh();
+    }
+
+    private void refresh() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(Duration.seconds(0), e -> updateTime()),
+                new KeyFrame(Duration.seconds(1))
+        );
+        timeline.setCycleCount(javafx.animation.Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    private void updateTime() {
+        LocalTime time = LocalTime.now();
+        String str = String.format(Locale.GERMAN, "%02d:%02d", time.getHour(), time.getMinute());
+        getChildren().setAll(font.render(str));
+    }
+}
--- src/main/java/de/schmizzolin/uhr/Main.java
+++ src/main/java/de/schmizzolin/uhr/Main.java
@@ -1,41 +1,22 @@
 package de.schmizzolin.uhr;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderStroke;
-import javafx.scene.layout.BorderStrokeStyle;
-import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-import javafx.util.Duration;
-
-import java.time.LocalTime;
-import java.util.Locale;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) {
-        StackPane root = new StackPane();
-        root.setStyle("-fx-background-color: black;");
-        root.setAlignment(Pos.CENTER);
-        root.setBorder(new Border(new BorderStroke(
-                Color.BLACK,
-                BorderStrokeStyle.SOLID,
-                null,
-                new BorderWidths(10)
-        )));
+        StackPane clock = new Clock();
 
-        Scene scene = new Scene(root, 360, 120);
+        Scene scene = new Scene(clock, 360, 120);
         stage.setTitle("Uhr");
         scene.setFill(Color.BLACK);
         stage.setScene(scene);
@@ -43,24 +24,10 @@ public class Main extends Application {
         stage.setAlwaysOnTop(true);
         stage.initStyle(StageStyle.UNDECORATED);
 
-        contextMenu(scene, root);
-        movable(stage, root);
-        refresh(root, Font.create(Color.WHITE));
+        contextMenu(scene, clock);
+        movable(stage, clock);
 
         stage.show();
-    }
-
-    private void refresh(StackPane root, Font font) {
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.seconds(0), e -> {
-                    LocalTime time = LocalTime.now();
-                    String str = String.format(Locale.GERMAN, "%02d:%02d", time.getHour(), time.getMinute());
-                    root.getChildren().setAll(font.render(str));
-                }),
-                new KeyFrame(Duration.seconds(1))
-        );
-        timeline.setCycleCount(javafx.animation.Animation.INDEFINITE);
-        timeline.play();
     }
 
     private void contextMenu(Scene scene, StackPane root) {
